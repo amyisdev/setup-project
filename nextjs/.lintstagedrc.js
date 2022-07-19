ac02b00581e5c71d63b5c642c04ca821e8a2d2ce
Azmi Makarima <dev.amy.azmim@gmail.com>
2022-07-19 16:43:59 +0700

    update lintstaged for nextjs

--- nextjs/.lintstagedrc.js
+++ nextjs/.lintstagedrc.js
@@ -1,8 +1,8 @@
-const path = require('path');
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+const path = require('path')
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-};
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`,
+    `prettier --write ${filenames.join(' ')}`,
+  ],
+}
